016cd74616cba011e5fddc87f3f39343249ae071
frazy <frazy@thstack.com>
2015-05-27 07:43:56 -0700

    add web

---
+++ html-design/ashinamo_web/plugin/memory/chart_gauge-f691ebb1.js
@@ -0,0 +1,41 @@
+var chart = c3.generate({
+    data: {
+        columns: [
+            ['data', 0]
+        ],
+        type: 'gauge',
+        onclick: function (d, i) { console.log("onclick", d, i); },
+        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+    },
+    gauge: {
+//        label: {
+//            format: function(value, ratio) {
+//                return value;
+//            },
+//            show: false // to turn off the min/max labels.
+//        },
+//    min: 0, // 0 is default, //can handle negative min e.g. vacuum / voltage / current flow / rate of change
+//    max: 100, // 100 is default
+//    units: ' %',
+//    width: 39 // for adjusting arc thickness
+    },
+    color: {
+        pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'], // the three color levels for the percentage values.
+        threshold: {
+//            unit: 'value', // percentage is default
+//            max: 200, // 100 is default
+            values: [30, 60, 90, 100]
+        }
+    },
+    size: {
+        height: 180
+    }
+});
+
+setInterval(function () {
+    var data = Math.random()*100;
+    chart.load({
+        columns: [['data', data]]
+    });
+}, 1000);
